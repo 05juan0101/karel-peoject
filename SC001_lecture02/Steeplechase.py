af1c915f8e975569b4aef2455a452bf1e39b2805
05juan0101 <140373031+05juan0101@users.noreply.github.com>
2025-03-31 13:37:30 +0800

    Add files via upload

---
+++ SC001_lecture02/Steeplechase.py
@@ -0,0 +1,78 @@
+"""
+File: Steeplechase.py
+Name: TODO:
+---------------------------------
+TODO:
+"""
+
+from karel.stanfordkarel import *
+
+
+def main():
+    """
+    Karel crosses hurdles in a 12x12 world
+    with a for loop 
+    """
+    for i in range(11):
+       if front_is_clear():
+           move()
+       else:
+           jump()
+           turn_left()
+
+
+
+def jump():
+    """
+    pre-condition: karel is on the left side of the wall,facing east
+    post-condition:karel is on the right side of the wall
+    """
+    up()
+    cross()
+    down()
+
+
+def cross():
+    """
+    pre-condition: karel is on the left side of the wall,facing north
+    post-condition:karel is at the upper right ,facing south
+    """
+    turn_right()
+    move()
+    turn_right()
+
+
+def down():
+    """
+    pre-condition: karel is on the left side of the wall,facing south
+    post-condition:
+    """
+    while front_is_clear():
+        move()
+
+
+
+def up():
+    """
+    pre-condition: karel is on the left side of the wall,facing east
+    post-condition:karel is at the upper left of the wall,facing north
+    """
+    turn_left()
+    while not right_is_clear():
+        move()
+    #alternative:
+    #while not front_is_clear():
+        #turn_left()
+        #move()
+        #turn_right()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+# ----- DO NOT MODIFY CODE BELOW THIS LINE ----- #
+if __name__ == '__main__':
+    execute_karel_task(main)
